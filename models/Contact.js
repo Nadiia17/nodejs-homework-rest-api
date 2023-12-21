0d666469641e621e1d775ff7f89286556f0112ab
Nadiia17 <128908761+Nadiia17@users.noreply.github.com>
2023-12-21 17:29:48 +0200

    upd

---
+++ models/Contact.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+const contsctSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Contact = model("contact", contsctSchema);
+
+export default Contact;
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const { DB_HOST, PORT = 3000 } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Database connection successful. Use our API on port: ${PORT}`);
     });
   })
   .catch((error) => {
